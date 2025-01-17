2d3e05aab62d9ae2f6be7a20bc8d83e43dafe0a0
mintesnot <mintesnotyess@gmail.com>
2025-01-18 02:32:14 +0300

    edit and add js some function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,10 @@ let AlbemArt = document.querySelector('.albem-art');
 let MusicTitle = document.querySelector('.music-title');
 let MusicPlayDev = document.querySelector('.MusicPlayDiv');
 let DocumentTitle = document.querySelector('title');
+let SongList = document.querySelector('.song-list');
+let PlayList = document.querySelector('.play-list');
+
+
 
 // musics and albem art file arrey list
 let musicFiles = [
@@ -78,3 +82,87 @@ audio.addEventListener('play', function () {
 audio.addEventListener('pause', function () {
     MusicPlayDev.innerHTML = '<i class="fa-solid fa-play"></i>';
 })
+
+
+
+// music 
+
+
+musicFiles.forEach((song, index) => {
+    const li = document.createElement('li');
+    li.innerHTML = `
+      <div class="song-info">
+        <div class="img">
+          <div class="play-button" data-index="${index}">
+            <span><i class="fa fa-play"></i></span>
+          </div>
+          <img src="${song.albumArt}" alt="" class="album-art">
+        </div>
+        <p class="title">${song.title}</p>
+      </div>
+    `;
+    SongList.appendChild(li);
+});
+
+const playButtons = document.querySelectorAll('.play-button');
+
+playButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const index = parseInt(button.getAttribute('data-index'));
+        const song = musicFiles[index];
+
+        if (audio.src === song.file && audio.paused) {
+            audio.play();
+            button.querySelector('i').classList.remove('fa-play');
+            button.querySelector('i').classList.add('fa-pause');
+        } else {
+            audio.src = song.file;
+            audio.play();
+            playButtons.forEach((btn) => {
+                btn.querySelector('i').classList.remove('fa-pause');
+                btn.querySelector('i').classList.add('fa-play');
+            });
+            button.querySelector('i').classList.remove('fa-play');
+            button.querySelector('i').classList.add('fa-pause');
+        }
+
+        MusicTitle.innerHTML = song.title;
+        AlbemArt.style.background = `url(${song.albumArt})center/cover `;
+        DocumentTitle.innerHTML = 'Shemane ⏺ ' + song.title;
+    });
+});
+
+audio.addEventListener('pause', () => {
+    playButtons.forEach((button) => {
+        if (button.querySelector('i').classList.contains('fa-pause')) {
+            button.querySelector('i').classList.remove('fa-pause');
+            button.querySelector('i').classList.add('fa-play');
+        }
+    });
+});
+
+PlayList.style.display = "none";
+function PlayListToggle() {
+    if (PlayList.style.display == "none") {
+        PlayList.style.opacity = '0';
+        PlayList.style.display = 'flex';
+
+        setInterval(() => {
+            PlayList.style.opacity = '1';
+
+        })
+
+
+
+    } else {
+        PlayList.style.display = "none";
+
+
+
+    }
+
+}
+
+PlayList.addEventListener('blur', () => {
+    PlayList.style.display = 'none';
+});
